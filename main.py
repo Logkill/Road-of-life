50d1246ced0742ee5d9f3bef60f60cb2ad4c0b56
ANEN <84581961+DangerousANEN@users.noreply.github.com>
2023-02-06 17:18:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
-import sys
 import numpy as np
 import pygame as pg
 from pygame import time
+
+import sys
 
 
 def load_image(name):
@@ -13,6 +14,13 @@ def load_image(name):
         print('Не могу загрузить изображение:', name)
         raise SystemExit(error)
     return image
+
+
+def load_level(filename):
+    filename = os.path.join('data', filename)
+    with open(filename, 'r') as map_file:
+        map_level = np.array([list(i) for i in [line.strip() for line in map_file]])
+    return map_level
 
 
 class Player(pg.sprite.Sprite):
@@ -26,22 +34,38 @@ class Player(pg.sprite.Sprite):
         )
 
     def move(self, x, y):
+
+        camera.dx -= tile_width * (x - self.pos[0])
+        camera.dy -= tile_height * (y - self.pos[1])
         self.pos = (x, y)
+        for tile in tiles_group:
+            camera.apply(tile)
         self.rect = self.image.get_rect().move(
             tile_width * self.pos[0] + 15,
-            tile_height * self.pos[1] + 5
+            tile_height * 6 + 5
         )
 
 
-def load_level(filename):
-    filename = os.path.join('data', filename)
-    with open(filename, 'r') as map_file:
-        map_level = np.array([list(i) for i in [line.strip() for line in map_file]])
-    return map_level
+class Car(pg.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(cars_group)
+        self.image = car_image
+        self.pos = (pos_x, pos_y)
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 1,
+            tile_height * pos_y + 1
+        )
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.rect = self.image.get_rect().move(
+            tile_width * self.pos[0] + 1,
+            tile_height * self.pos[1] + 1
+        )
 
 
 def generate_level(level):
-    hero, x, y = None, None, None
+    mas, hero, x, y = None, None, None, None
     row, col = level.shape
     for y in range(row):
         for x in range(col):
@@ -50,99 +74,111 @@ def generate_level(level):
             elif level[y, x] == '#':
                 Tile('road', x, y)
                 level[y, x] = '.'
+            elif level[y, x] == '0':
+                Tile('rir', x, y)
+                level[y, x] = '0'
             elif level[y, x] == ',':
-                Tile('car', x, y)
+                Tile('empty', x, y)
+                level[y, x] = ','
+                mas = Car(x, y)
             elif level[y, x] == '@':
                 Tile('empty', x, y)
                 level[y, x] = '.'
                 hero = Player(x, y)
-    return hero, x, y
+    return mas, hero, x, y
+
+
+class Camera:
+    def __init__(self):
+        self.dx, self.dy = 0, 0
+
+    def apply(self, obj):
+        obj.rect.y = obj.abs_pos[1] + self.dy
+
+    def update(self):
+        self.dx, self.dy = 0, 0
 
 
 class Tile(pg.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group)
         self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x,
-            tile_height * pos_y
-        )
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        self.abs_pos = (self.rect.x, self.rect.y)
 
 
 def start_screen():
-    """Вывод заставки игры"""
-    intro_text = ["Лучший результат: "]
-    # Выводим изображение заставки:
-    start_screen_background = load_image('start.jpg')
-    screen.blit(start_screen_background, (0, 0))
-    # Выводим текст заставки:
-    font = pg.font.Font(None, 40)
-    text_coord = 420
-    for line in intro_text:
-        string_rendered = font.render(line, True, pg.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 500
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-    # Главный игровой цикл для окна заставки:
+    screen.fill(pg.Color('White'))
+    screen.blit(load_image('start.jpg'), (0, 0))
     while True:
         for event_game in pg.event.get():
             if event_game.type == pg.QUIT:
                 terminate()
             elif event_game.type == pg.KEYDOWN or event_game.type == pg.MOUSEBUTTONDOWN:
-                return  # Начинаем игру
+                return
         pg.display.flip()
 
 
 def move_player(hero, movement):
     x, y = hero.pos
     if movement == 'up':
-        if y > 0 and level_map[y - 1, x] == '.':
+        if y > 0 and level_map[y - 1, x] == '.' or (y > 0 and level_map[y - 1, x] == ','):
             hero.move(x, y - 1)
     elif movement == 'down':
-        if y < level_y and level_map[y + 1, x] == '.':
+        if y < level_y and level_map[y + 1, x] == '.' or (y < level_y and level_map[y + 1, x] == ','):
             hero.move(x, y + 1)
     elif movement == 'left':
-        if x > 0 and level_map[y, x - 1] == '.':
+        if x > 0 and level_map[y, x - 1] == '.' or (x > 0 and level_map[y, x - 1] == ','):
             hero.move(x - 1, y)
     elif movement == 'right':
-        if x < level_x and level_map[y, x + 1] == '.':
+        if x < level_x and level_map[y, x + 1] == '.' or (x < level_x and level_map[y, x + 1] == ','):
             hero.move(x + 1, y)
 
-            
+
+def move_car(mas):
+    x, y = mas.pos
+    if (x < level_x and level_map[y, x + 1] == '.') or (x < level_x and level_map[y, x + 1] == ','):
+        mas.move(x + 1, y)
+    elif x < level_x and level_map[y, x + 1] == '0' or x >= level_x:
+        mas.move(0, y)
+
+
 def terminate():
-    """Выход из игры"""
-    # Определяем отдельную функцию выхода из игры,
-    # чтобы ею можно было воспользоваться,
-    # как при закрытии игрового окна,
-    # так и заставки:
     pg.quit()
     sys.exit()
-    
+
+
 if __name__ == '__main__':
     pg.init()
+    fps = 60
     size = width, height = 1100, 750
-    screen = pg.display.set_mode((size))
+    screen = pg.display.set_mode(size)
+
     player_image = load_image('chicken.png')
+    cars_image = load_image('car.png')
+
     tile_images = {
+        'rir': load_image('water.png'),
         'road': load_image('road.png'),
-        'empty': load_image('trava.png'),
-        'car': load_image('car.png')
+        'empty': load_image('trava.png')
     }
+
     tile_width = tile_height = 50
+
+    cars_group = pg.sprite.Group()
     player_group = pg.sprite.Group()
     tiles_group = pg.sprite.Group()
 
     pg.display.set_caption("Road-of-Life")
     chicken_image = pg.image.load("data/chicken.png")
     car_image = pg.image.load("data/car.png")
-    fps = 60
+
     level_map = load_level('level-01.map')
     pg.key.set_repeat(200, 70)
-    player, level_x, level_y = generate_level(level_map)
+    m, player, level_x, level_y = generate_level(level_map)
 
+    camera = Camera()
+    start_menu = True
     start_screen()
 
     running = True
@@ -152,7 +188,9 @@ if __name__ == '__main__':
                 running = False
 
             elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP:
+                if event.key == pg.K_SPACE:
+                    start_menu = False
+                elif event.key == pg.K_UP:
                     move_player(player, 'up')
                 elif event.key == pg.K_DOWN:
                     move_player(player, 'down')
@@ -160,13 +198,11 @@ if __name__ == '__main__':
                     move_player(player, 'left')
                 elif event.key == pg.K_RIGHT:
                     move_player(player, 'right')
-
-            screen.fill(pg.Color('white'))
-            tiles_group.draw(screen)
-            player_group.draw(screen)
-            
-            time.Clock().tick(fps)
-            pg.display.flip()
+            if not start_menu:
+                tiles_group.draw(screen)
+                player_group.draw(screen)
+                cars_group.draw(screen)
 
         time.Clock().tick(fps)
+        pg.display.flip()
 
